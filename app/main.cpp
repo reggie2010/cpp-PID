a8e3bdedc362cf8f76dff7d175cde54c8b423cfe
senthilarul <senthilhariharana@gmail.com>
2017-09-26 08:30:57 -0700

    added command to print new velocity

--- app/main.cpp
+++ app/main.cpp
@@ -11,7 +11,13 @@ int main() {
     ControllerPID controller;
     controller.setKI(0.5);
     controller.setKI(0.25);
-    controller.setKD(0.1);
+    controller.setKD(0.01);
+    //controller.setKI(1);
+    //controller.setKI(0.5);
+    //controller.setKD(0.0001);
 
+//
+controller.outputPID(6.0,0.5,0.1);
+//
     return 0;
 }

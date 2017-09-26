dafaea7392ce1f900dda68b7a80823ddbbbd1b53
senthilarul <senthilhariharana@gmail.com>
2017-09-26 08:32:47 -0700

    added pid computation to outputPID

--- app/pid.cpp
+++ app/pid.cpp
@@ -5,6 +5,9 @@
 
 #include <iostream>
 #include "pid.hpp"
+#include <cmath>
+#include <chrono>
+#include <thread>
 
 ControllerPID::ControllerPID() : mkp(0.0), mki(0.0), mkd(0.0) {
 	/*
@@ -37,6 +40,28 @@ double ControllerPID::outputPID(double refSig, double feedbackSig, double timeDe
 	*/
 
 	double error {refSig - feedbackSig};
-
-	return 0.0;
+//
+    double totalError =0;
+    double previousError = 0;
+    double newVelocity = 0;
+    double kp = getKP();
+    double ki = getKI();
+    double kd = getKD();
+    //std::cout << kp <<" "<< ki<<" "<<kd<<"\t";
+    while ( std::abs(error) > 0.01 ) {
+    error = refSig - feedbackSig;
+    //std::cout << "error: "<< error <<std::endl;
+    totalError += error;
+    //std::cout << kp * error << " "<< ki * totalError *timeDelta << " "<< ((error - previousError)/timeDelta) * kd << std::endl;
+    double newVel = kp * error + ki * totalError *timeDelta + ((error - previousError)/timeDelta) * kd;
+    previousError = error;
+    newVelocity = (newVel > 10) ? 10 : newVel < -10 ? -10 : newVel;
+    
+//newVelocity = newVel;    
+feedbackSig = newVelocity;
+    //std::this_thread::sleep_for (std::chrono::seconds(1));
+}
+//      
+        std::cout<<error<<" "<<newVelocity<<std::endl;
+	return newVelocity;
 }
